7c10f3f2cd4acab0593d75f7a76eaf01189584f2
Ben-vegabond <vega.ben18@gmail.com>
2023-06-10 10:50:27 -0500

    started the foundation

---
+++ script.js
@@ -0,0 +1,15 @@
+const quizData = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Paris", "London", "Madrid", "Rome"],
+      answer: "Paris"
+    },
+    {
+      question: "Which programming language is used to build web pages?",
+      choices: ["HTML", "CSS", "JavaScript", "Python"],
+      answer: "HTML"
+    },
+    // Add more questions and answers here
+  ];
+  
+  
